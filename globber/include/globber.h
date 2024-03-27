5098a84b3c6d321529fe5827a2f407c6f5ca6a3d
Dan Pike <danny.pike@gmail.com>
2024-03-27 07:42:22 +0000

    Added command-line options; output banner

--- globber/include/globber.h
+++ globber/include/globber.h
@@ -3,6 +3,8 @@
 
 namespace mikado::globber {
 
+   const unsigned int GLOBBER_VERSION_MAJOR = 0;
+   const unsigned int GLOBBER_VERSION_MINOR = 1;
     
 } // namespace mikado::globber
 
--- globber/src/main.cpp
+++ globber/src/main.cpp
@@ -5,6 +5,7 @@
 #include "globber.h"
 
 namespace common = mikado::common;
+namespace po = boost::program_options;
 namespace windowsApi = mikado::windowsApi;
 typedef common::MikadoErrorCode MikadoErrorCode;
 using namespace std;
@@ -14,7 +15,44 @@ namespace mikado::globber {
 
    //////////////////////////////////////////////////////////////////////////
    //
-   MikadoErrorCode main(int argc, char *argv[]) {
+   static void outputBanner() {
+      str_notice() << "Globber version " << GLOBBER_VERSION_MAJOR << "." << GLOBBER_VERSION_MINOR
+         << " (" << __DATE__ << " " << __TIME__ << ")" << endl;
+      str_notice() << "Copyright (c) 2024 Gamaliel Ltd" << endl << endl;
+   }
+
+   //////////////////////////////////////////////////////////////////////////
+   //
+   static MikadoErrorCode main(int argc, char *argv[]) {
+
+      common::MikadoLog::MikadoLogger.setOutputStdout(false);
+      outputBanner();
+
+      po::options_description options;
+      options.add_options()
+         ("include", po::value<vector<string>>()->multitoken(), "include a file or folder (regex)")
+         ("exclude", po::value<vector<string>>()->multitoken(), "exclude a file or folder (regex)")
+         ("default", po::value<string>(), "default action (include|exclude)")
+         ("help", "produce help message")
+         ;
+
+      po::variables_map args;
+      store(po::command_line_parser(argc, argv).
+         options(options).run(), args);
+      notify(args);
+
+      // If anything asks for help, that's the only thing we do
+      if (args.count("help")) {
+         common::MikadoLog::MikadoLogger.setOutputODS(false);
+         common::MikadoLog::MikadoLogger.setOutputStdout(true);
+         outputBanner();
+         str_notice() << "Usage: globber [options]" << endl << endl
+            << "Where [options] are:" << endl;
+         cout << options;
+         return MikadoErrorCode::MKO_ERROR_NONE;
+      }
+
+
       return MikadoErrorCode::MKO_ERROR_NONE;
    }
     
