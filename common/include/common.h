95e45136e30053cb6be67e9674c39b74cbe39687
Dan Pike <danny.pike@gmail.com>
2024-03-28 05:37:22 +0000

    dead code

--- common/include/common.h
+++ common/include/common.h
@@ -7,12 +7,7 @@ namespace mikado::common {}
 
 #include "common/algorithms.h"
 #include "common/ascii.h"
-#include "common/database.h"
 #include "common/errorCodes.h"
 #include "common/logger.h"
-#include "common/startProcess.h"
-#include "common/windowsApi.h"
-#include "common/windowsHandle.h"
-#include "common/windowsPipe.h"
 
 #endif // MKO_COMMON_H
