6a642ab9e243e0be446ae5b79ca214d758d87f2c
Dan Pike <danny.pike@gmail.com>
2024-04-13 07:53:49 +0100

    added simple toString() that displays the tensor as a flat blob of values, with start & count indices

--- torchBox/include/torchBox/pytorch.h
+++ torchBox/include/torchBox/pytorch.h
@@ -6,7 +6,39 @@
 namespace mikado::torchBox {
 
    std::vector<size_t> getShape(torch::Tensor const &tensor);
+   
+   ///////////////////////////////////////////////////////////////////////////
+   //
+   template <class TT>
+   std::string toString(torch::Tensor const &tensor, int64_t count = -1, int64_t start = 0) {
+      std::stringstream ss;
+      try
+      {
+         auto view = tensor.view({ -1 });
+         auto sizes = view.sizes();
+         auto end = sizes[0] - 1;
+         auto from = std::min(std::max((int64_t)0, start), end);
+         auto to = (count < 0) ? end : std::min(std::max(from, start + count - 1), end);
 
+         auto data = view.accessor<TT, 1>();
+         ss << "[";
+         for (int64_t ii = from; ii <= to; ++ii) {
+            if (from < ii) {
+               ss << ", ";
+            }
+            ss << data[ii];
+         }
+         if (to < end) {
+            ss << ", ...";
+         }
+         ss << "]";
+      }
+      catch (const std::exception &e)
+      {
+         log_exception(e);
+      }
+      return ss.str();
+   }
 } // namespace mikado::common
 
 #endif // TBOX_PYTORCH_H
--- torchBox/src/pytorch.cpp
+++ torchBox/src/pytorch.cpp
@@ -1,10 +1,13 @@
 // Copyright (c) 2024 Gamaliel Ltd
+#include "common.h"
 #include "torchBox/pytorch.h"
 
 using namespace std;
 
 namespace mikado::torchBox {
 
+   ///////////////////////////////////////////////////////////////////////////
+   //
    vector<size_t> getShape(torch::Tensor const &tensor) {
       vector<size_t> shape;
       for (auto dim : tensor.sizes()) {
