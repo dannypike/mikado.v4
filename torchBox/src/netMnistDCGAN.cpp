5b2f129adaaa01c6f77fe1170345bb5cb369ee43
Dan Pike <danny.pike@gmail.com>
2024-05-08 07:42:43 +0100

    dead code

--- torchBox/src/netMnistDCGAN.cpp
+++ torchBox/src/netMnistDCGAN.cpp
@@ -1,5 +1,4 @@
 #include "common.h"
-#include "torchBox/dcganGeneratorImpl.h"
 #include "torchBox/netMnistDCGAN.h"
 
 using namespace std;
@@ -8,89 +7,89 @@ namespace bt = boost::posix_time;
 namespace common = mikado::common;
 namespace po = boost::program_options;
 
-// The size of the noise vector fed to the generator.
-const int64_t kNoiseSize = 100;
-
-// The batch size for training.
-const int64_t kBatchSize = 64;
-
-// After how many batches to create a new checkpoint periodically.
-const int64_t kCheckpointEvery = 200;
-
-// How many images to sample at every checkpoint.
-const int64_t kNumberOfSamplesPerCheckpoint = 10;
-
-// Set to `true` to restore models and optimizers from previously saved
-// checkpoints.
-const bool kRestoreFromCheckpoint = false;
-
-// After how many batches to log a new update with the loss value.
-const int64_t kLogInterval = 10;
-
-struct DCGANGeneratorImpl : nn::Module {
-   DCGANGeneratorImpl(int kNoiseSize)
-      : conv1(nn::ConvTranspose2dOptions(kNoiseSize, 256, 4)
-         .bias(false)),
-      batch_norm1(256),
-      conv2(nn::ConvTranspose2dOptions(256, 128, 3)
-         .stride(2)
-         .padding(1)
-         .bias(false)),
-      batch_norm2(128),
-      conv3(nn::ConvTranspose2dOptions(128, 64, 4)
-         .stride(2)
-         .padding(1)
-         .bias(false)),
-      batch_norm3(64),
-      conv4(nn::ConvTranspose2dOptions(64, 1, 4)
-         .stride(2)
-         .padding(1)
-         .bias(false))
-   {
-      // register_module() is needed if we want to use the parameters() method later on
-      register_module("conv1", conv1);
-      register_module("conv2", conv2);
-      register_module("conv3", conv3);
-      register_module("conv4", conv4);
-      register_module("batch_norm1", batch_norm1);
-      register_module("batch_norm2", batch_norm2);
-      register_module("batch_norm3", batch_norm3);
-   }
-
-   torch::Tensor forward(torch::Tensor x) {
-      x = torch::relu(batch_norm1(conv1(x)));
-      x = torch::relu(batch_norm2(conv2(x)));
-      x = torch::relu(batch_norm3(conv3(x)));
-      x = torch::tanh(conv4(x));
-      return x;
-   }
-
-   nn::ConvTranspose2d conv1, conv2, conv3, conv4;
-   nn::BatchNorm2d batch_norm1, batch_norm2, batch_norm3;
-}; // struct DCGANGeneratorImp
-
-TORCH_MODULE(DCGANGenerator);
-
-nn::Sequential create_discriminator() {
-   return nn::Sequential(
-      // Layer 1
-      nn::Conv2d(nn::Conv2dOptions(1, 64, 4).stride(2).padding(1).bias(false)),
-      nn::LeakyReLU(nn::LeakyReLUOptions().negative_slope(0.2)),
-      // Layer 2
-      nn::Conv2d(nn::Conv2dOptions(64, 128, 4).stride(2).padding(1).bias(false)),
-      nn::BatchNorm2d(128),
-      nn::LeakyReLU(nn::LeakyReLUOptions().negative_slope(0.2)),
-      // Layer 3
-      nn::Conv2d(
-         nn::Conv2dOptions(128, 256, 4).stride(2).padding(1).bias(false)),
-      nn::BatchNorm2d(256),
-      nn::LeakyReLU(nn::LeakyReLUOptions().negative_slope(0.2)),
-      // Layer 4
-      nn::Conv2d(nn::Conv2dOptions(256, 1, 3).stride(1).padding(0).bias(false)),
-      nn::Sigmoid());
-}
-
 namespace mikado::torchBox {
+
+   // The size of the noise vector fed to the generator.
+   const int64_t kNoiseSize = 100;
+
+   // The batch size for training.
+   const int64_t kBatchSize = 64;
+
+   // After how many batches to create a new checkpoint periodically.
+   const int64_t kCheckpointEvery = 200;
+
+   // How many images to sample at every checkpoint.
+   const int64_t kNumberOfSamplesPerCheckpoint = 10;
+
+   // Set to `true` to restore models and optimizers from previously saved
+   // checkpoints.
+   const bool kRestoreFromCheckpoint = false;
+
+   // After how many batches to log a new update with the loss value.
+   const int64_t kLogInterval = 10;
+
+   struct DCGANGeneratorImpl : nn::Module {
+      DCGANGeneratorImpl(int kNoiseSize)
+         : conv1(nn::ConvTranspose2dOptions(kNoiseSize, 256, 4)
+            .bias(false)),
+         batch_norm1(256),
+         conv2(nn::ConvTranspose2dOptions(256, 128, 3)
+            .stride(2)
+            .padding(1)
+            .bias(false)),
+         batch_norm2(128),
+         conv3(nn::ConvTranspose2dOptions(128, 64, 4)
+            .stride(2)
+            .padding(1)
+            .bias(false)),
+         batch_norm3(64),
+         conv4(nn::ConvTranspose2dOptions(64, 1, 4)
+            .stride(2)
+            .padding(1)
+            .bias(false))
+      {
+         // register_module() is needed if we want to use the parameters() method later on
+         register_module("conv1", conv1);
+         register_module("conv2", conv2);
+         register_module("conv3", conv3);
+         register_module("conv4", conv4);
+         register_module("batch_norm1", batch_norm1);
+         register_module("batch_norm2", batch_norm2);
+         register_module("batch_norm3", batch_norm3);
+      }
+
+      torch::Tensor forward(torch::Tensor x) {
+         x = torch::relu(batch_norm1(conv1(x)));
+         x = torch::relu(batch_norm2(conv2(x)));
+         x = torch::relu(batch_norm3(conv3(x)));
+         x = torch::tanh(conv4(x));
+         return x;
+      }
+
+      nn::ConvTranspose2d conv1, conv2, conv3, conv4;
+      nn::BatchNorm2d batch_norm1, batch_norm2, batch_norm3;
+   }; // struct DCGANGeneratorImp
+
+   TORCH_MODULE(DCGANGenerator);
+
+   nn::Sequential create_discriminator() {
+      return nn::Sequential(
+         // Layer 1
+         nn::Conv2d(nn::Conv2dOptions(1, 64, 4).stride(2).padding(1).bias(false)),
+         nn::LeakyReLU(nn::LeakyReLUOptions().negative_slope(0.2)),
+         // Layer 2
+         nn::Conv2d(nn::Conv2dOptions(64, 128, 4).stride(2).padding(1).bias(false)),
+         nn::BatchNorm2d(128),
+         nn::LeakyReLU(nn::LeakyReLUOptions().negative_slope(0.2)),
+         // Layer 3
+         nn::Conv2d(
+            nn::Conv2dOptions(128, 256, 4).stride(2).padding(1).bias(false)),
+         nn::BatchNorm2d(256),
+         nn::LeakyReLU(nn::LeakyReLUOptions().negative_slope(0.2)),
+         // Layer 4
+         nn::Conv2d(nn::Conv2dOptions(256, 1, 3).stride(1).padding(0).bias(false)),
+         nn::Sigmoid());
+   }
 
    typedef common::MikadoErrorCode MikadoErrorCode;
 
